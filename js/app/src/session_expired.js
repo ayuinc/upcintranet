db9bf1c5cad1bf1621241939108fc33a3bb94c21
Gianfranco Montoya <gms122@gmail.com>
2014-12-15 15:50:36 -0500

    login redirect

--- js/app/src/session_expired.js
+++ js/app/src/session_expired.js
@@ -2,5 +2,5 @@
 $(document).ready(function(){
 	setTimeout(function () {
        window.location.href = "/login/salir"; //will redirect to your blog page (an ex: blog.html)
-    }, 30*60*1000);
+    }, 10*1000);
 });
