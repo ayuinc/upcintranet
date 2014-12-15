89b08290f4738cb313eeb1cf9defd32e65beaf47
Gianfranco Montoya <gms122@gmail.com>
2014-12-15 15:48:27 -0500

    login redirect

--- js/app/src/session_expired.js
+++ js/app/src/session_expired.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
 	setTimeout(function () {
-       window.location.href = "/?ACT=28&return=login%2Findex"; //will redirect to your blog page (an ex: blog.html)
-    }, 10*1000);
+       window.location.href = "/login/salir"; //will redirect to your blog page (an ex: blog.html)
+    }, 30*60*1000);
 });
