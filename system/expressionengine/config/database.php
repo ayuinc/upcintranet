fb67e355bc7c523a3b2697b7269a34bc1e895d8e
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2014-12-30 18:11:49 -0500

    configuracion local

--- system/expressionengine/config/database.php
+++ system/expressionengine/config/database.php
@@ -3,10 +3,10 @@
 $active_group = 'expressionengine';
 $active_record = TRUE;
 
-$db['expressionengine']['hostname'] = 'localhost';
+$db['expressionengine']['hostname'] = '127.0.0.1';
 $db['expressionengine']['username'] = 'root';
 // $db['expressionengine']['password'] = '';
-$db['expressionengine']['password'] = 'JKPQiUAb8f';
+$db['expressionengine']['password'] = '';
 $db['expressionengine']['database'] = 'UPCIntranet';
 $db['expressionengine']['dbdriver'] = 'mysql';
 $db['expressionengine']['pconnect'] = FALSE;
