bc965f2188342197d4e87acbe0b204bb27b6aaea
Andrea Del Rio <delrio.andre@gmail.com>
2015-01-22 12:49:39 -0500

    working on search icon in header

--- js/app/src/global.js
+++ js/app/src/global.js
@@ -4,4 +4,8 @@ $(document).ready(function(){
         $(this).siblings('.parent').find('ul').slideUp();
         $(this).find('.child').slideToggle();
     });
+    $('.search-trigger').click(function(){
+    	$('.search-bar').toggleClass('hidden');
+    	$('.search-icon').toggleClass('hidden');
+    })
 });
