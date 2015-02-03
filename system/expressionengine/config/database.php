c96218ba7e0d497c8eb488058b87296dffa140b4
Andrea Del Rio <delrio.andre@gmail.com>
2015-02-03 18:41:30 -0500

    updated links to sidebard docentes"

--- system/expressionengine/config/database.php
+++ system/expressionengine/config/database.php
@@ -3,9 +3,11 @@
 $active_group = 'expressionengine';
 $active_record = TRUE;
 
-$db['expressionengine']['hostname'] = 'ip-172-31-52-134.ec2.internal';
-$db['expressionengine']['username'] = 'ayuinc';
-$db['expressionengine']['password'] = 'ayuinc2014';
+// $db['expressionengine']['hostname'] = 'ip-172-31-52-134.ec2.internal';
+$db['expressionengine']['hostname'] = '127.0.0.1';
+// $db['expressionengine']['username'] = 'ayuinc';
+$db['expressionengine']['username'] = 'root';
+$db['expressionengine']['password'] = '';
 // $db['expressionengine']['password'] = 'ayuinc2014';
 $db['expressionengine']['database'] = 'upc';
 $db['expressionengine']['dbdriver'] = 'mysql';
