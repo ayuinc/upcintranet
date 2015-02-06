6299ea91b5bab7c6201fc33a9c2402013b544922
Andrea Del Rio <delrio.andre@gmail.com>
2015-02-06 18:54:07 -0500

    trying to fill in white space between content and footer

--- js/app/src/global.js
+++ js/app/src/global.js
@@ -2,4 +2,6 @@ $(document).ready(function(){
     $('.search-trigger').click(function(){
     	$('.search-bar').toggleClass('hidden');
     })
+    targetHeight = $('.site-content').height() + $('.internal-footer').height() - 18;
+    $('.site-menu').css("height",targetHeight + "px");
 });
