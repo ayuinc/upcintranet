d437f20f2fcdae2ea1a5bdd0542a7863e7f145bb
Andrea Del Rio <delrio.andre@gmail.com>
2015-02-27 18:46:39 -0500

    removing fixed from internal footer

--- js/app/src/large_sidebar.js
+++ js/app/src/large_sidebar.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     // dynamic site-menu height
     // if ($('.site-content').height() > 571) {
-	    targetHeight = $('.site-content').height() + $('.internal-footer').height() + 91;
+	    targetHeight = $('.site-content').height() + 35;
 	    $('.site-menu').css("height",targetHeight + "px");
     // }
 });
