59e7bcc60e9d56e53795e2dc00c226b401c9d348
Gianfranco Montoya <gms122@gmail.com>
2015-03-10 15:28:37 -0500

    =)

--- system/expressionengine/third_party/webservices/pi.webservices.php
+++ system/expressionengine/third_party/webservices/pi.webservices.php
@@ -3424,7 +3424,7 @@ class Webservices
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
       curl_setopt($ch, CURLOPT_URL,$url);
       $result=curl_exec($ch);
-      var_dump($result);
+      //var_dump($result);
       $json = json_decode($result, true);
       
       $result = '';
