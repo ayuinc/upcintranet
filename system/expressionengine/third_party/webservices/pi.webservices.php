f8706f66b1e3391777a03bbdb57ef116e433f372
Gianfranco Montoya <gms122@gmail.com>
2015-03-17 16:05:01 -0500

    =)

--- system/expressionengine/third_party/webservices/pi.webservices.php
+++ system/expressionengine/third_party/webservices/pi.webservices.php
@@ -72,7 +72,7 @@ class Webservices
       //$url = urlencode($contrasena);
       //$codigo = 'u412701';
       //$contrasena = '8Ki#3Ygt';
-      $contrasena = to_utf8($contrasena);
+      $contrasena = utf8_encode($contrasena);
       var_dump($contrasena);
       
       $url = 'https://upcmovil.upc.edu.pe/upcmovil1/UPCMobile.svc/Autenticar2/?Codigo='.$codigo.'&Contrasena='.$contrasena.'&Plataforma='.$plataforma;
