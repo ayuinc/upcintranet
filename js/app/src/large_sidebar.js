445865652590187b1ee4057d2da7e5b9bdda2161
Andrea Del Rio <delrio.andre@gmail.com>
2015-03-18 17:26:56 -0500

    portrait

--- js/app/src/large_sidebar.js
+++ js/app/src/large_sidebar.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     // dynamic site-menu height
     // if ($('.site-content').height() > 571) {
-	    targetHeight = $('.site-content').height() + 49;
+	    targetHeight = $('.site-content').height();
 	    $('.site-menu').css("height",targetHeight + "px");
     // }
 });
