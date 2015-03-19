4219c2d95a0397eaf8ac0511f44a7c1257c44149
Andrea Del Rio <delrio.andre@gmail.com>
2015-03-19 18:11:05 -0500

    new design

--- js/app/src/large_sidebar.js
+++ js/app/src/large_sidebar.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     // dynamic site-menu height
     // if ($('.site-content').height() > 571) {
-	    targetHeight = $('.site-content').height() + 49;
+	    targetHeight = $('.site-content').height() - 100;
 	    $('.site-menu').css("height",targetHeight + "px");
     // }
 });
