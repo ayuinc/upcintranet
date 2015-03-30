8791c933e2cabd615d87d7365458b79ea0024cd0
Gianfranco Montoya <gms122@gmail.com>
2015-03-30 18:24:07 -0500

    =)

--- system/expressionengine/third_party/webservices/pi.webservices.php
+++ system/expressionengine/third_party/webservices/pi.webservices.php
@@ -987,7 +987,7 @@ class Webservices
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
       curl_setopt($ch, CURLOPT_URL,$url);
       $result=curl_exec($ch);
-      var_dump($result);
+      //var_dump($result);
       $json = json_decode($result, true);
       
       $error = $json['CodError'];
@@ -1530,7 +1530,7 @@ class Webservices
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
       curl_setopt($ch, CURLOPT_URL,$url);
       $result=curl_exec($ch);
-      var_dump($result);
+      //var_dump($result);
       $json = json_decode($result, true);
       
       $error = $json['CodError'];
