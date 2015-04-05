092db0304ce5862c777e17ebd16a0048249ddbd0
andreadelrio <delrio.andre@gmail.com>
2015-04-04 20:23:59 -0500

    cambios solicitados frontend

--- js/app/src/login.js
+++ js/app/src/login.js
@@ -5,4 +5,21 @@ $(document).ready(function(){
   if ($('.site-content').hasClass("dashboard")) {
   	$('.site-wrapper').addClass("grid-bg");
   };
+  //validate login form
+	$(".login-form").validate({
+	  // Specify the validation rules
+    rules: {
+      codigo: "required",
+      contrasena: "required",
+    },
+    // Specify the validation error messages
+    messages: {
+      codigo: {
+          required: "Debes ingresar tu código",
+      },
+      contrasena: {
+          required: "Debes ingresa tu contraseña",
+      },
+    },
+	});
 });
