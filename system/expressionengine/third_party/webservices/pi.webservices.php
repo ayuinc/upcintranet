e229fd15b15178c333969923abf88f7c7dc4244a
Gianfranco Montoya <gms122@gmail.com>
2015-04-07 01:29:34 -0500

    =)

--- system/expressionengine/third_party/webservices/pi.webservices.php
+++ system/expressionengine/third_party/webservices/pi.webservices.php
@@ -75,7 +75,6 @@ class Webservices
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
       curl_setopt($ch, CURLOPT_URL,$url);
       $result=curl_exec($ch);
-      $result='{"Codigo":null,"CodigoAlumno":null,"Nombres":null,"Apellidos":null,"Genero":null,"EsAlumno":null,"Estado":null,"TipoUser":null,"Token":null,"Datos":null,"CodError":"null","MsgError":"Usuario y\/o contraseña incorrectos."}';
       $json = json_decode($result, true);
       setcookie("MsgError", $json['MsgError'], time() + (1800), "/");
       $_SESSION["CodError"] = $json['CodError'];
