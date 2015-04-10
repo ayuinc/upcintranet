4bbe4dfb2939ac51315d7db14b394c2308388ce3
Gianfranco Montoya <gms122@gmail.com>
2015-04-10 01:04:08 -0500

    test ciclo actual padres

--- system/expressionengine/third_party/webservices/pi.webservices.php
+++ system/expressionengine/third_party/webservices/pi.webservices.php
@@ -719,7 +719,7 @@ class Webservices
         $dia_actual = date('w');
         var_dump($dia_actual);
         //Despliega solo las clases del dia
-         if ($json['HorarioDia'][$i]['CodDia']==date('w')) {
+         if ($json['HorarioDia'][$i]['CodDia']==7) {
             $result .= '  <div class="panel-body-head-table">
                           <ul class="tr">
                             <li class="col-xs-2">
