8c3c2ab0646b47e9ff036202d6aeff5a3b319df2
andreadelrio <delrio.andre@gmail.com>
2015-04-28 14:19:14 -0700

    dropdowns ie8

--- js/app/src/ie8.js
+++ js/app/src/ie8.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$menuWidth = $('.site-menu').width();
 	$menuWidthPx = $menuWidth + "px";
-	console.log($menuWidthPx);
+	// console.log($menuWidthPx);
 	$('.site-content').css('width', '100%').css('width', "-=" + $menuWidthPx);
 	$("#ie-avatar").hover(function(){
 		$(this).attr("src","http://10.0.0.21/assets/img/user_ie8_info.png");
