ead7abb8de72a04e54d03f073ea90151fadea10f
andreadelrio <delrio.andre@gmail.com>
2015-04-28 18:34:56 -0700

    dashboard estudiantes

--- system/expressionengine/third_party/webservices/pi.webservices.php
+++ system/expressionengine/third_party/webservices/pi.webservices.php
@@ -1185,10 +1185,10 @@ class Webservices
          $result = '<div class="panel-body-head-table">';
          $result .= '<div class="panel-table mis-cursos-content" id="miscursos">';
          $result .= '<ul class="tr">';
-         $result .= '<li class="col-xs-4">';
+         $result .= '<li class="col-sm-4">';
          $result .= '<img class="img-center" src="{site_url}assets/img/no_courses_new.png">';
          $result .= '</li>';
-         $result .= '<li class="col-xs-8 pt-28 pr-21">';
+         $result .= '<li class="col-sm-8 pt-28 pr-21">';
          $result .= '<p class="zizou-bold-16">¿Ningún curso en este Ciclo?</p>';
          $result .= '<p class="helvetica-14">Entérate de <a href="#" class="sb-link">otras</a> actividades que puedes realizar o <a href="" class="sb-link">reinicia tu cursada</a></p>';
          $result .= '</li>';
@@ -1246,10 +1246,10 @@ class Webservices
         $result = '<div class="panel-body-head-table">';
         $result .= '<div class="panel-table mis-cursos-content" id="miscursos">';
         $result .= '<ul class="tr">';
-        $result .= '<li class="col-xs-4">';
+        $result .= '<li class="col-sm-4">';
         $result .= '<img class="img-center" src="{site_url}assets/img/no_courses_new.png">';
         $result .= '</li>';
-        $result .= '<li class="col-xs-8 pt-28 pr-21">';
+        $result .= '<li class="col-sm-8 pt-28 pr-21">';
         if ($error_mensaje == "Ud. no se encuentra matriculado en el presente ciclo.") {
         $result .= '<p class="zizou-bold-16">¿Ningún curso en este Ciclo?</p>';
         $result .= '<p class="helvetica-14">Entérate de <a href="#" class="sb-link">otras</a> actividades que puedes realizar o <a href="" class="sb-link">reinicia tu cursada</a></p>';
@@ -3667,13 +3667,13 @@ class Webservices
         $result = '<div class="panel-body">';
         $result .= '<div class="panel-table">';
         $result .= '<ul class="tr">';
-        $result .= '<li class="col-xs-4">';
+        $result .= '<li class="col-sm-4">';
         $result .= '<img class="img-center" src="{site_url}assets/img/no_bookings_new.png">';
         $result .= '</li>';
         if ($error_mensaje == "No se han registrado reservas durante esta semana.") {
-        $result .= '<li class="col-xs-8 pt-21 pr-21"><p class="helvetica-14">Reserva de <a href="#" class="danger-link">cubículos, </a><a href="#" class="danger-link">computadoras</a> o <a href="#" class="danger-link">espacios deportivos</a></p></li>'; 
+        $result .= '<li class="col-sm-8 pt-21 pr-21"><p class="helvetica-14">Reserva de <a href="#" class="danger-link">cubículos, </a><a href="#" class="danger-link">computadoras</a> o <a href="#" class="danger-link">espacios deportivos</a></p></li>'; 
         } else {
-        $result .= '<li class="col-xs-8 pt-28 pr-21"><p class="helvetica-14">'.$error_mensaje.'</p></li>'; 
+        $result .= '<li class="col-sm-8 pt-28 pr-21"><p class="helvetica-14">'.$error_mensaje.'</p></li>'; 
         }
         $result .= '</ul>';
         $result .= '</div>';
