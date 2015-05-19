ee571814d8b8e79cf5f0db3dc068eab8a06abaaa
Elizabeth Portilla Flores <darknessover136@gmail.com>
2015-05-18 20:19:43 -0500

    reservas validation by require_from_group

--- js/app/src/reservas.js
+++ js/app/src/reservas.js
@@ -1,61 +1,76 @@
-$(document).ready(function(){
-  //selects
-  // $('.selectpicker').selectpicker();
-  //dropdowns
-	$(".datepicker").datepicker({
-		dateFormat: 'dd/mm/yy',
-		minDate: new Date(),
-    maxDate: '+1D',
-	});
-	$("#deportivos-form").validate({
+jQuery.validator.addMethod("require_from_group", function(value, element, options) {
+  var numberRequired = options[0];
+  var selector = options[1];
+  var fields = $(selector, element.form);
+  var filled_fields = fields.filter(function() {
+    // it's more clear to compare with empty string
+    return ($(this).val() !== "" && $(this).val() !== null); 
+  });
+  var empty_fields = fields.not(filled_fields);
+  // we will mark only first empty field as invalid
+  if (filled_fields.length < numberRequired && empty_fields[0] === element) {
+    return false;
+  }
+  return true;
+  // {0} below is the 0th item in the options field
+}, jQuery.validator.format("Debes seleccionar un espacio"));
+
+$(document).ready(function () {
+ 
+  $(".datepicker").datepicker({
+    dateFormat : 'dd/mm/yy',
+    minDate : new Date(),
+    maxDate : '+1D',
+  });
+  $("#deportivos-form").validate({
     // Ignore not visible fields 
     // ignore:":not(:visible)",
-      ignore : [],
-	  // Specify the validation rules
+    ignore : [],
+    // Specify the validation rules
     rules: {
       FechaIni: "required",
       CodSede: "required",
-      CodED: {require_from_group:[1, 'data-id^=CodED']}
-      CodED1:{require_from_group:[1, 'data-id^=CodED']}
-      CodED2: {require_from_group:[1, 'data-id^=CodED']}
-      CodActiv: "required",
+      CodED : {require_from_group : [1, '.espacios-deportivos']},
+      CodED1 : {require_from_group : [1, '.espacios-deportivos']},
+      CodED2 : {require_from_group : [1, '.espacios-deportivos']},
+      CodActiv : "required",
 
     },
     // Specify the validation error messages
     messages: {
-        FechaIni: {
-            required: "Debes seleccionar una fecha",
-        },
-        CodSede: {
-            required: "Debes seleccionar una sede",
-        },
-        CodED: {
-            required: "Debes seleccionar un espacio",
-        },
-        CodED1: {
-            required: "Debes seleccionar un espacio",
-        },
-        CodED2: {
-            required: "Debes seleccionar un espacio",
-        },
+      FechaIni: {
+        required: "Debes seleccionar una fecha",
+      },
+      CodSede: {
+        required: "Debes seleccionar una sede",
+      },
+      CodED: {
+        required: "Debes seleccionar un espacio",
+      },
+      CodED1: {
+        required: "Debes seleccionar un espacio",
+      },
+      CodED2: {
+        required: "Debes seleccionar un espacio",
+      },
 
     },
-    errorPlacement: function(error, element) {
-      if(element.hasClass('selectpicker')){
-        shadowElement = $('[data-id="'+element.attr('id')+'"]').parent();
-        if(shadowElement.is(':visible')){
-          error.insertAfter(shadowElement); 
-        }else{
-         return;
+    errorPlacement: function (error, element) {
+      if (element.hasClass('selectpicker')) {
+        var shadowElement = $('[data-id="' + element.attr('id') + '"]').parent();
+        if (shadowElement.is(':visible')) {
+          error.insertAfter(shadowElement);
+        } else {
+          return;
         }
-      }else if(element.is(':visible')){
-         error.insertAfter(element);
+      } else if (element.is(':visible')) {
+        error.insertAfter(element);
       }
       return;
     }
-	});
-	$("#computadoras-form").validate({
-		// Specify the validation rules
+  });
+  $("#computadoras-form").validate({
+  // Specify the validation rules
     rules: {
       CodSede: "required",
       CanHoras: "required",
@@ -64,22 +79,22 @@ $(document).ready(function(){
     },
     // Specify the validation error messages
     messages: {
-        FecIni: {
-            required: "Debes seleccionar una fecha",
-        },
-        CodSede: {
-            required: "Debes seleccionar una sede",
-        },
-        CanHoras: {
-            required: "Debes seleccionar la cantidad de horas",
-        },
-        HoraIni: {
-            required: "Debes seleccionar la hora inicial",
-        },
+      FecIni: {
+        required: "Debes seleccionar una fecha",
+      },
+      CodSede: {
+        required: "Debes seleccionar una sede",
+      },
+      CanHoras: {
+        required: "Debes seleccionar la cantidad de horas",
+      },
+      HoraIni: {
+        required: "Debes seleccionar la hora inicial",
+      },
     },
-	});
-	$("#cubiculos-form").validate({
-	  // Specify the validation rules
+  });
+  $("#cubiculos-form").validate({
+  // Specify the validation rules
     rules: {
       CodSede: "required",
       FecIni: "required",
@@ -89,17 +104,19 @@ $(document).ready(function(){
     // Specify the validation error messages
     messages: {
       FecIni: {
-          required: "Debes seleccionar una fecha",
+        required: "Debes seleccionar una fecha",
       },
       CodSede: {
-          required: "Debes seleccionar una sede",
+        required: "Debes seleccionar una sede",
       },
       CanHoras: {
-          required: "Debes seleccionar la cantidad de horas",
+        required: "Debes seleccionar la cantidad de horas",
       },
       HoraIni: {
-          required: "Debes seleccionar la hora inicial",
+        required: "Debes seleccionar la hora inicial",
       },
     },
-	});
+  });
+
+  //add require from group method
 });
