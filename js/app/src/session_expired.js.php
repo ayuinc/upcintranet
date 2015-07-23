fcf72956f7652f01684305e193ae6bf3c4705f20
Ivandevp <medinadiazivan@gmail.com>
2015-07-23 13:48:23 -0500

    ALL3-2, ALL3-3 manejar tiempos de sesión a 30 minutos

--- js/app/src/session_expired.js.php
+++ js/app/src/session_expired.js.php
@@ -2,6 +2,6 @@ var site_url = <?php  echo $_GET['url']; ?>;
 
 $(document).ready(function(){
 	setTimeout(function () {
-       window.location.href = site_url + "login/salir";
+       window.location.href = site_url + "general/session-expired";
     }, 30*60*1000);
 });
