c61dcceb7ea0bd7fbea0e614e29651a872f10c70
Elizabeth Portilla Flores <darknessover136@gmail.com>
2015-08-26 17:35:16 -0500

    fix diseño de terminos y condiciones en login

--- js/app/src/login.js
+++ js/app/src/login.js
@@ -1,14 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function () {
   if ($('.site-content').hasClass("login")) {
-  	$('.site-wrapper').addClass("grid-bg");
-  };
+    $('.site-wrapper').addClass("grid-bg");
+  }
   if ($('.site-content').hasClass("dashboard")) {
-  	$('.site-wrapper').addClass("grid-bg");
-  };
+    $('.site-wrapper').addClass("grid-bg");
+  }
   //validate login form
-	$(".login-form").validate({
-    // var form = this; 
-	  // Specify the validation rules
+  $(".login-form").validate({
     rules: {
       codigo: "required",
       contrasena: "required",
@@ -16,24 +14,36 @@ $(document).ready(function(){
     // Specify the validation error messages
     messages: {
       codigo: {
-          required: "Debes ingresar tu código de usuario",
+        required: "Debes ingresar tu código de usuario",
       },
       contrasena: {
-          required: "Debes ingresar tu contraseña",
+        required: "Debes ingresar tu contraseña",
       },
     },
-    submitHandler: function($this) {
+    submitHandler: function ($this) {
     // do other things for a valid form
-    document.cookie = "onLogin=true; ";
+      document.cookie = "onLogin=true; ";
     // form.submit();
-  }
-	});
+    }
+  });
+});
+
+$(document).ready(function () {
+  $('#checkbox').change(function () {
+    if($('#checkbox').is(':checked')){
+      $('#aceptar').attr("disabled", false);
+    }else{
+      $('#aceptar').attr("disabled", true);
+    }
+   
+  });
+
 });
 
 
-function validateSource(){
-  if(getCookie("onLogin")==false){
-    var url = "/";    
-    $(location).attr('href',url);
+function validateSource() {
+  if( getCookie("onLogin") === false) {
+    var url = "/";
+    $(location).attr('href', url);
   }
 }
