0489c4e91d2a261733d593623d411fdd83e0f1ee
Elizabeth Portilla Flores <darknessover136@gmail.com>
2015-09-14 23:00:38 -0500

    Arreglar los links de buscar, que sean links validos

--- js/app/src/login.js
+++ js/app/src/login.js
@@ -25,37 +25,36 @@ $(document).ready(function () {
       document.cookie = "onLogin=true; ";
     // TERMINOS Y CONDICIONES 
       var form = $this;
-      // event.preventDefault();
-      // var hostname = ''; 
-      // var codigo = $("input[name='codigo']").val();
+      event.preventDefault();
+      var hostname = ''; 
+      var codigo = $("input[name='codigo']").val();
 
-      // $.ajax({
-      //   url: hostname +'includes/terminos_condiciones_get',
-      //   type: "POST",
-      //   data: ({codigo: codigo}),
-      //   success: function(data){
-      //     var result = data.slice(0,2);
-      //     if (result == 'no') {
-      //       $('#condicionesModal').modal('show');
-      //       $( "#aceptar" ).click(function() {
-      //         if ($('#checkbox').is(':checked')) {
-      //           $.ajax({
-      //             url: hostname +'/includes/terminos_condiciones_set',
-      //             type: "POST",
-      //             data: ({codigo: codigo}),
-      //             success: function(data){
-      //               form.submit();      
-      //             }
-      //           });
-      //         }  
-      //       });
-      //     }else 
-      //       form.submit();       
+      $.ajax({
+        url: hostname +'includes/terminos_condiciones_get',
+        type: "POST",
+        data: ({codigo: codigo}),
+        success: function(data){
+          var result = data.slice(0,2);
+          if (result == 'no') {
+            $('#condicionesModal').modal('show');
+            $( "#aceptar" ).click(function() {
+              if ($('#checkbox').is(':checked')) {
+                $.ajax({
+                  url: hostname +'/includes/terminos_condiciones_set',
+                  type: "POST",
+                  data: ({codigo: codigo}),
+                  success: function(data){
+                    form.submit();      
+                  }
+                });
+              }  
+            });
+          }else 
+            form.submit();       
           
-      //   }
-      // });
+        }
+      });
       // END TERMINOS Y CONDICIONES
-      form.submit();  
     }
   });
 });
