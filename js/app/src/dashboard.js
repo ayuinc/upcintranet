36af948981a197b2d9a02f339b68c5f05a346368
Elizabeth Portilla Flores <darknessover136@gmail.com>
2015-10-26 17:19:04 -0500

    tutorial message

--- js/app/src/dashboard.js
+++ js/app/src/dashboard.js
@@ -17,8 +17,16 @@ $(document).ready(function(){
 		$(".msge-row").hide();
 	}
 
-	$('#myModal').modal('toggle');
 
+	jQuery.each($('ul.slides li img'), function(i, val){
+	 	$(val).load(function(){ 
+	 		console.log('cargó!'+i);
+	 	 	$('#load-tutorial-message').hide();
+	 	 }).error(function(){
+	 	 	console.log('xeess!'+i);
+	 	 });
+	});
+	$('#myModal').modal('toggle');
 });
 
 function getCookie(cname) {
