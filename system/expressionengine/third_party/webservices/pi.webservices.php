e550ab78b8b24c5e6a1ff4725c58ed02ff61639d
Elizabeth Portilla Flores <darknessover136@gmail.com>
2016-03-28 14:50:05 -0500

    Fix de bloqueo para  Gimnasio Monterrico

--- system/expressionengine/third_party/webservices/pi.webservices.php
+++ system/expressionengine/third_party/webservices/pi.webservices.php
@@ -3980,7 +3980,12 @@ class Webservices
             $hora_inicio_disp = substr($json['HorarioDia'][$i]['Disponibles'][$a]["HoraFin"],0,2);
             $hora_inicio_fal = substr($json['HorarioDia'][$i]['Disponibles'][$a]["HoraInicio"],0,2);
             $hora_inicio_sol = substr($HoraIni,0,2);
-            if(($json['HorarioDia'][$i]['CodDia'] == 1 || $json['HorarioDia'][$i]['CodDia'] == 3 || $json['HorarioDia'][$i]['CodDia'] == 6) && $json['HorarioDia'][$i]['Disponibles'][$a]['Sede'] == 'R' && $coded == 181 && !(($hora_inicio_disp<=13 && $hora_inicio_fal<=13) || ($hora_inicio_disp>=14 && $hora_inicio_fal>=14))){
+            if(
+               (($json['HorarioDia'][$i]['CodDia'] == 1 || $json['HorarioDia'][$i]['CodDia'] == 3 || $json['HorarioDia'][$i]['CodDia'] == 6) && $json['HorarioDia'][$i]['Disponibles'][$a]['Sede'] == 'R' && $coded == 181 && !(($hora_inicio_disp<=13 && $hora_inicio_fal<=13) || ($hora_inicio_disp>=14 && $hora_inicio_fal>=14))) ||
+                ($json['HorarioDia'][$i]['Disponibles'][$a]['Sede'] == 'R' && $coded == 101 && !(($hora_inicio_disp<=9 && $hora_inicio_fal<=9) || ($hora_inicio_disp>=10 && $hora_inicio_fal>=10))) || 
+                (($json['HorarioDia'][$i]['CodDia'] != 6) && $json['HorarioDia'][$i]['Disponibles'][$a]['Sede'] == 'L' && $coded == 101 && !(($hora_inicio_disp<=15 && $hora_inicio_fal<=15) || ($hora_inicio_disp>=16 && $hora_inicio_fal>=16)) && !(($hora_inicio_disp<=22 && $hora_inicio_fal<=22) || ($hora_inicio_disp>=23 && $hora_inicio_fal>=23))) ||
+                ($json['HorarioDia'][$i]['CodDia'] == 6 && $json['HorarioDia'][$i]['Disponibles'][$a]['Sede'] == 'L' && $coded == 101 && !(($hora_inicio_disp<=15 && $hora_inicio_fal<=15) || ($hora_inicio_disp>=23 && $hora_inicio_fal>=23))) 
+                )  {
               // var_dump("jaskdaldka;ld");
               // var_dump('Inicio dis'.$hora_inicio_disp.' numhoras '.$numhoras.' Inicio fal '.$hora_inicio_fal);
             }else{
