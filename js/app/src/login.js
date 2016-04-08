c3dabe1765e75aaafcaad3c02be0e50d35ad544a
Elizabeth Portilla Flores <darknessover136@gmail.com>
2016-04-08 08:28:18 -0500

    login fix

--- js/app/src/login.js
+++ js/app/src/login.js
@@ -54,15 +54,16 @@ $(document).ready(function () {
   if(isMobile == true){
       $('input[name="codigo"]').on('keyup', function (event) {
         console.log('key up');
-        if(($(this).val()[0]== 'u' ||  $(this).val()[0]== 'U') && $(this).val().length==1){
+        if(($(this).val()[0]== 'u' ||  $(this).val()[0]== 'U') && $(this).val().length<=1){
           $(this).prop('type', 'tel');
           $(this).blur(); 
           $(this).focus();
-        }else if($(this).val().length==1){
+        }else if($(this).val().length<=1){
           $(this).prop('type', 'text');
           $(this).blur(); 
           $(this).focus();
         }
+        $(this).focus();
       });
       $('input[name="contrasena"]').on('focus', function (event) {
         $('input[name="codigo"]').prop('type', 'text');
