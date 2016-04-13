96645ca4cc997d70867034f00501be6f9f49a300
Elizabeth Portilla Flores <darknessover136@gmail.com>
2016-04-13 15:21:58 -0500

    Prueba de verificar token en dev

--- system/expressionengine/third_party/webservices/pi.webservices.php
+++ system/expressionengine/third_party/webservices/pi.webservices.php
@@ -5725,7 +5725,7 @@ class Webservices
 
       $result = $this->services->curl_full_url(ee()->config->item('verification_services_url').'/'.$codigo.'/'.$token,  ee()->config->item('verification_user'),  ee()->config->item('verification_pwd'));
 
-      if ($codigo === '' || is_null($codigo) || is_null($terminos) || $terminos == 'no' || $terminos == '' && $verification_result['DTOHeader']['CodigoRetorno'] == "Correcto") {
+      if (($codigo === '' || is_null($codigo) || is_null($terminos) || $terminos == 'no' || $terminos == '') || $verification_result['DTOHeader']['CodigoRetorno'] !== "Correcto")) {
         $redireccion = uri_string();
         $this->eliminar_cookie();
         $_COOKIE[$this->services->get_fuzzy_name("Redireccion")] = $redireccion;
