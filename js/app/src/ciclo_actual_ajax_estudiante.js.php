bc9a9760692bb022a568ec8ddb56e1cd29e84660
Elizabeth Portilla Flores <darknessover136@gmail.com>
2016-04-19 18:07:02 -0500

    Encuestas: pasar a AJAX el horario

---
+++ js/app/src/ciclo_actual_ajax_estudiante.js.php
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+    var href = window.location.protocol + "://" + window.location.host + window.location.pathname;
+    var codigo_alumno = href.substr(href.lastIndexOf('/') + 1);
+    var hostname = <?php  echo $_GET['url']; ?>;
+
+    $.ajax({
+        url: hostname +'includes/mis-estudios-cicloactual',
+        type: "GET",
+        data: ({codigo_alumno: codigo_alumno}),
+        success: function(data){
+
+            $('#cargador-ciclo-actual-estudiante').remove();
+            $( "#mi-ciclo-actual" ).append( $(data) );
+            if($('input.session-expired-redirect').size()!=0){
+                window.location = '/general/session-expired';
+            }
+        }
+    });
+
+});
