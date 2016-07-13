d14a6e110c88e818d62cc9eb3aed47a121a4cd60
Elizabeth Portilla Flores <darknessover136@gmail.com>
2016-07-12 20:34:08 -0500

    error

--- system/expressionengine/third_party/webservices/pi.webservices.php
+++ system/expressionengine/third_party/webservices/pi.webservices.php
@@ -1397,7 +1397,7 @@ class Webservices
                                 } else {
 //                                    $this->services->upc_log("ENCUESTAS;" . $codigo . ";" . $quiz_request . ";" . $result . ";" . date('ddmmyyyy - H:i:s') . "\n", "logs/ENCUESTAS.log");
 
-                                    $form = "<!-- " . $quiz_request . " COD ERROR" . $qjson['CodError'] . " --><a class=\"inactive\" href=\"#\"><span class=\"helvetica-14\"><img src=\"" . $site_url . 'assets/img/btn-encuesta-no-disponible.jpg' . "\" alt=\"Encuesta\"></span></a>";
+                                    $form = "<a class=\"inactive\" href=\"#\"><span class=\"helvetica-14\"><img src=\"" . $site_url . 'assets/img/btn-encuesta-no-disponible.jpg' . "\" alt=\"Encuesta\"></span></a>";
                                     $horario_dia = $this->tags->replace_subtag_data('survey_form', $horario_dia, $form);
                                 }
                             } else {
