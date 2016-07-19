56b9df1f8440aab059c52c2e2275ed2d6ae04b21
Elizabeth Portilla <darknessover136@gmail.com>
2016-07-19 00:49:46 +0000

    Pruebas encuestas 4

--- system/expressionengine/third_party/webservices/pi.webservices.php
+++ system/expressionengine/third_party/webservices/pi.webservices.php
@@ -1482,10 +1482,10 @@ class Webservices
             }
         }
         if($i != null){
-        //   return $this->tags->get_subtag_data('no_encuestas', $tagdata);
-            $error_result = $this->tags->get_subtag_data('error', $this->EE->TMPL->tagdata);
-            $error_result = $this->tags->replace_subtag_data('error_message', $error_result, $date->format('N'));
-            return $error_result;
+            return $this->tags->get_subtag_data('no_encuestas', $tagdata);
+            // $error_result = $this->tags->get_subtag_data('error', $this->EE->TMPL->tagdata);
+            // $error_result = $this->tags->replace_subtag_data('error_message', $error_result, $date->format('N'));
+            // return $error_result;
         }
         $horario = $horario_empty;
         //genera el tamano del array
