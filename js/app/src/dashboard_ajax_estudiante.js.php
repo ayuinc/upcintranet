4c1f2976f4eb146edf6f54158502bab4c9fdcddd
Denys <denysrondan@gmail.com>
2016-08-04 23:00:27 +0000

    prueba:validacion de actualizacion de datos

--- js/app/src/dashboard_ajax_estudiante.js.php
+++ js/app/src/dashboard_ajax_estudiante.js.php
@@ -127,13 +127,12 @@ $(document).ready(function () {
             $('#modalactualizar').modal('toggle');
         }
         $('#form-actualizar').validate({
-
             rules:{
                 email: {
                     required : data.obligatorio,
                     maxlength: 50,
                     notUPCemail : true,
-                    email : true,
+                    email : true
                 },
                 phone : {
                     required : data.obligatorio,
@@ -158,7 +157,7 @@ $(document).ready(function () {
                     required : data.ApodEmailOblig,
                     maxlength: 50,
                     notUPCemail : true,
-                    email : true,
+                    email : true
                 },
                 phoneApoderado : {
                     required : data.ApodMovilOblig,
@@ -182,7 +181,6 @@ $(document).ready(function () {
                     "number": "Solo ingrese números",
                     "minlength": "Debe tener 9 dígitos",
                     "maxlength": "Ingrese un número de teléfono móvil válido"
-
                 },
                 nombreApoderado : {
                     "required"  : "Ingrese el nombre del Apoderado"
@@ -213,7 +211,9 @@ $(document).ready(function () {
                 $.post(hostname + 'includes/actualizar-datos-post', {
                     phone : $('#form-actualizar #phone').val(),
                     email : $('#form-actualizar #email').val(),
+                    
                     codigoAp : $('#form-actualizar #codigoAp').val(),
+                    
                     nombreApoderado : $('#form-actualizar #nombreApoderado').val(),
                     apellidoPatApoderado : $('#form-actualizar #apellidoPatApoderado').val(),
                     apellidoMatApoderado : $('#form-actualizar #apellidoMatApoderado').val(),
